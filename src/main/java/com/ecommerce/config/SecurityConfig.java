51a2fb5e07c5efceba42001de624272fccef64ea
Luc Van Dat <n18dccn038@student.ptithcm.edu.vn>
2022-04-14 22:33:05 +0700

    update

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -91,8 +92,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
